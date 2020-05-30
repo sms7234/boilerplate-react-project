0b8ddb39d5f2161e11ed43893baa283d5df23e6e
Shane Sloan <sloan.shane.1991@gmail.com>
2020-05-30 17:15:49 -0600

    Corrected copy & paste errors.  Now a single line of text shows

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {Provider} from 'react-redux'
 import ReactDOM from 'react-dom';
 import AppRouter, {history} from './router/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
 
 
 ReactDOM.render(<AppRouter />,document.querySelector('#app'));
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Router, Route, Switch, Link, NavLink} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import DashboardPage from '../components/DashboardPage';
 import NotFoundPage from '../components/NotFoundPage';
 
