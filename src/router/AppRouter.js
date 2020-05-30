f459af349b9093c5850c2053e5643eb191baa60b
Shane Sloan <sloan.shane.1991@gmail.com>
2020-05-30 16:29:28 -0600

    Inital Commit

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import {Router, Route, Switch, Link, NavLink} from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
 import DashboardPage from '../components/DashboardPage';
-import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -13,9 +9,8 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <PublicRoute path="/" component={LoginPage} exact={true}/>
-        <PrivateRoute path="/dashboard" component={DashboardPage}/>
-        <PrivateRoute path="/create" />
+        <Route path="/" component={DashboardPage} />
+        <Route component={NotFoundPage} />
       </Switch>
     </div>
   </Router>
